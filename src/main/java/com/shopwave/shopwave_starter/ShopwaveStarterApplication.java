bdce4ae547e7bbe190565b32bd25aff2fba5801f
kase2228 <etelethiopia@gmail.com>
2026-03-21 16:46:43 +0300

    other project contents and configurations

---
+++ src/main/java/com/shopwave/shopwave_starter/ShopwaveStarterApplication.java
@@ -0,0 +1,14 @@
+//ATE/8400/14
+package com.shopwave.shopwave_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopwaveStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopwaveStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopwave/shopwave_starter/ShopwaveStarterApplicationTests.java
@@ -0,0 +1,14 @@
+//ATE/8400/14
+package com.shopwave.shopwave_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopwaveStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
