97ea833480d1399a1d81fd6daed55e934f0e0db4
kase2228 <etelethiopia@gmail.com>
2026-03-21 17:31:34 +0300

    Configure main application to scan components outside its package

--- src/main/java/com/shopwave/shopwave_starter/ShopwaveStarterApplication.java
+++ src/main/java/com/shopwave/shopwave_starter/ShopwaveStarterApplication.java
@@ -3,8 +3,10 @@ package com.shopwave.shopwave_starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.shopwave")
 public class ShopwaveStarterApplication {
 
 	public static void main(String[] args) {
